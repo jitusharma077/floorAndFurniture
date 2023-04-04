e5a7f64ddcf82d1ac2add8e53d2c57d4f574466f
Jitu <Jeetsharma753@gmail.com>
2023-04-04 11:50:11 +0530

    chnages with search in all enquiryes

--- src/Components/Pages/SuperAdmin/AllEnquiry.js
+++ src/Components/Pages/SuperAdmin/AllEnquiry.js
@@ -1,6 +1,7 @@
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Button } from "reactstrap";
 import { GetDataWithToken } from "../../ApiHelper/ApiHelper";
 import Loader from "../../Common/Loader";
 import PaginationComponent from "../../Common/PaginationComponent";
@@ -16,6 +17,7 @@ function AllEnquiry() {
   const [data, setdata] = useState([]);
   const [isLoading, setisLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [SearchValue, setSearchValue] = useState("");
 
   const handlePageClick = (e, index) => {
     e.preventDefault();
@@ -35,6 +37,19 @@ function AllEnquiry() {
       }
     );
   }, [currentPage]);
+
+  const getSearchValue = (val) => {
+    setisLoading(true);
+    GetDataWithToken(
+      `superadmin/search-enquiry/?id=${SearchValue?.target?.value}`
+    ).then((response) => {
+      if (response.status === true) {
+        setdata(response.data);
+        setisLoading(false);
+        settotalPage(response.pages);
+      }
+    });
+  };
 
   return (
     <>
@@ -64,10 +79,26 @@ function AllEnquiry() {
               <div className="col-12">
                 <div className="card">
                   <div className="card-header">
-                    <h4 className="card-title">All Enquiry</h4>
-                    <Link to={"/AddRooms"} className="btn btn-primary">
-                      Add New Enquiry
-                    </Link>
+                    <div className="col-lg-3">
+                      <h4 className="card-title">All Enquiry</h4>
+                    </div>
+                    <div className="col-lg-5 d-flex">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search enquiry by ID"
+                        value={SearchValue?.target?.value}
+                        onChange={(e) => {
+                          setSearchValue(e);
+                        }}
+                      />
+                      <button
+                        className="btn btn-primary ms-2"
+                        onClick={() => getSearchValue()}
+                      >
+                        Search
+                      </button>
+                    </div>
                   </div>
                   <div className="card-body">
                     <div className="table-responsive">
